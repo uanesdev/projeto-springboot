02a2f81e72eca40ad283a604ce8c177f77ac60d0
Uan <uanesfrancisco@gmail.com>
2025-05-06 19:25:18 -0300

    Inicialiazndo o projeto

---
+++ src/main/java/dev/uan/projetospringboot/ProjetospringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.uan.projetospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetospringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/uan/projetospringboot/ProjetospringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.uan.projetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetospringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
